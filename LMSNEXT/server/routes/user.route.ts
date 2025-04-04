93ca19ad285d0a9640649d4d7df9447f999649f9
vishaljailiya <vv700650@gmail.com>
2025-04-04 16:19:14 +0530

    course model done

--- LMSNEXT/server/routes/user.route.ts
+++ LMSNEXT/server/routes/user.route.ts
@@ -9,6 +9,7 @@ import {
   socialAuth,
   updateAccessToken,
   updatePassword,
+  updateProfilePicture,
   updateUserInfo,
 } from "../controller/user.controller";
 import { isAuthenticated } from "../Middleware/Auth";
@@ -25,5 +26,6 @@ userRouter.get("/me", isAuthenticated, getUserInfo);
 userRouter.post("/social-auth", socialAuth);
 userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
 userRouter.put("/update-user-password", isAuthenticated, updatePassword);
+userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture);
 
 export default userRouter;
--- LMSNEXT/server/server.ts
+++ LMSNEXT/server/server.ts
@@ -1,10 +1,17 @@
 import { log } from "console";
 import { app } from "./app";
+import { v2 as cloudinary } from "cloudinary";
 import connectDB from "./utils/db";
 import dotenv from "dotenv";
 dotenv.config();
 
 console.log("ACCESS_TOKEN:", process.env.ACCESS_TOKEN); // Debug log
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
+});
 
 // create server
 
